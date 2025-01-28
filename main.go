a8926d4c56d706bb1adbb241b0136bdf2938eb90
Artem-Sergeevi4 <nokia990@bk.ru>
2025-01-28 17:34:38 +0300

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,5 @@ func main() {
 }
 
 func hello() string {
-<<<<<<< HEAD
-	return "Hello Go"
+    return "Hello Go" // эту строку исправили Go теперь не go
 } 
->>>>>>> d10d2f4fcbe905d62f89817e4a3999b4b287fe63
